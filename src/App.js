d6be121bea2d7226c90bdd81346441c827f902aa
ScriptBoy0201 <50424@immanuel.qld.edu.au>
2025-05-18 11:27:33 +1000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import MindForgeDashboard from './MindForgeDashboard';
+
+function App() {
+  return <MindForgeDashboard />;
+}
+
+export default App;
---
+++ src/MindForgeDashboard.js
@@ -0,0 +1,1 @@
+<insert-dashboard-code-here>
